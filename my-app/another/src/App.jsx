3ebc7a9f031f75a1d816fca565354f854ec392cd
Naitik Singh <naitiksingh.fas@gmail.com>
2026-08-06 00:52:23 +0530

    Update App.jsx

--- my-app/another/src/App.jsx
+++ my-app/another/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
-import "./App.css";
+import "./app.css";
 import { ToastContainer } from "react-toastify";
 import Sidebar from "./sidebar/Sidebar";
 import Header from "./sidebar/Header";
